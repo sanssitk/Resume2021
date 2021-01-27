795676dc0c920336080c37cdc71ac1c7d8292e2d
sanssitk <sanssitk@gmail.com>
2021-01-26 23:25:37 -0600

    accordion function added

--- resume-2021/src/Components/Accordion/Accordion.js
+++ resume-2021/src/Components/Accordion/Accordion.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Accordion.css";
 import Chevron from "./Chevron";
 
 function Accordion(props) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [height, setHeight] = useState("0px"); 
+  const bodyContent = useRef(null);
+
+  useEffect(() => {
+    setIsOpen(true)
+  }, [])
+
+  const handleClick = () => {
+    setIsOpen(isOpen ? false : true);
+    setHeight(isOpen === true ? `${bodyContent.current.scrollHeight}px` : "0px");
+    console.log(bodyContent.current)
+  };
+
   return (
     <div className="accordion__Section">
       <div className="accordion__Section__header">
-        <div className="accordion__title__container">
+        <div className="accordion__title__container" onClick={handleClick}>
           <div className="accordion__name">
             <span className="icon" style={{ backgroundColor: props.color }}>
               {props.icon}
@@ -22,7 +36,13 @@ function Accordion(props) {
         </div>
         {props.header}
       </div>
-      <div className="accordion__body">{props.body}</div>
+      <div
+        ref={bodyContent}
+        className="accordion__body"
+        style={{ maxHeight: `${height}` }}
+      >
+        {props.body}
+      </div>
     </div>
   );
 }
