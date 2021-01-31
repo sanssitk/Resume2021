a6d14a2bbbada68799b66a586bce7b8041913e22
sanssitk <sanssitk@gmail.com>
2021-01-31 01:53:13 -0600

    cirular progress bar updated

--- resume-2021/src/Components/Resume/CircularProgress.js
+++ resume-2021/src/Components/Resume/CircularProgress.js
@@ -1,24 +1,54 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-function CircularProgress() {
-  const size = 170;
-  const stokeWidth = 40;
-  const circleOneColor = "#d9edfe";
-  const circleTwoColor = "rgb(255, 188, 56)";
+function CircularProgress({ bgcolor, completed }) {
+  const [offset, setOffset] = useState(132);
+  const circleRef = useRef(null);
+
+  const size = 50;
+  const progress = completed;
+  const stokeWidth = 8;
+  const circleOneColor = "rgb(224, 224, 222)";
+  const circleTwoColor = bgcolor;
 
   const center = size / 2;
   const radius = (size - stokeWidth) / 2;
   const circumference = 2 * Math.PI * radius;
 
+  useEffect(() => {
+    setInterval(() => {
+      const progressOffset = ((100 - progress) / 100) * circumference;
+      setOffset(progressOffset);
+    }, 1500);
+    circleRef.current.style = "transition: stroke-dashoffset 2.5s ease-in-out";
+    return () => clearInterval();    
+  }, [setOffset, progress, circumference, offset]);
+
   return (
     <div className="circularProgress__container">
-      <svg xmlns="http://www.w3.org/2000/svg" class="piechart">
-        <path d="M66,66 L130,66  A64,64 0 0,1 57,129 z" fill="#327ea3"></path>
-        <path d="M66,66 L57, 129 A64,64 0 0,1 2,60 z" fill="#4ca5d0"></path>
-        <path d="M66,66 L2,  60  A64,64 0 0,1 34,11 z" fill="#6ca338"></path>
-        <path d="M66,66 L34, 11  A64,64 0 0,1 103,14 z" fill="#ffbc38"></path>
-        <path d="M66,66 L103,14  A64,64 0 0,1 130,66 z" fill="#e82c0c"></path>
-        <circle cx="66" cy="66" r="30" fill="#ffffff"></circle>
+      <svg width={size} height={size} className="circularprogress" rotate="-90" origin={`${radius/2}, ${radius/2}`} >
+        <circle
+          className="circle-bg"
+          stroke={circleOneColor}
+          cx={center}
+          cy={center}
+          r={radius}
+          strokeWidth={stokeWidth - 2}
+        ></circle>
+        <circle
+          className="circle"
+          ref={circleRef}
+          stroke={circleTwoColor}
+          cx={center}
+          cy={center}
+          r={radius}
+          strokeWidth={stokeWidth}
+          strokeDasharray={circumference}
+          strokeDashoffset={offset}
+          // strokeLinecap="round"
+        ></circle>
+        <text x={center} y={center + 5} className="percentage">
+          {progress}%
+        </text>
       </svg>
     </div>
   );
